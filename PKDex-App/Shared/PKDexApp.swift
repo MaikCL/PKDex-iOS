c95afe5a051f2809666f139bce31fff42a305ab1
Miguel Angel <mzapatae@icloud.com>
2021-04-29 11:54:09 -0400

    Rearrange App module files
    Signed-off-by: Miguel Angel <mzapatae@icloud.com>

---
+++ PKDex-App/Shared/PKDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKDexApp.swift
+//  Shared
+//
+//  Created by Miguel Angel on 29-04-21.
+//
+
+import SwiftUI
+
+@main
+struct PKDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello world!!")
+        }
+    }
+}
