d896dca93d0a5835a409fbaf5cc612d9d76c4fbc
Miguel Angel Zapata <mzapatae@icloud.com>
2021-04-30 19:56:25 -0400

    Add cloud datasource
    Signed-off-by: Miguel Angel Zapata <mzapatae@icloud.com>

---
+++ Modules/Listing/Sources/Data/DataSources/Cloud/ListingCloudSource.swift
@@ -0,0 +1,20 @@
+//
+//  ListingCloudSource.swift
+//  
+//
+//  Created by Miguel Angel on 30-04-21.
+//
+
+import APIs
+import Combine
+import Resolver
+import AltairMDKProviders
+
+final class ListingCloudSource: ListingCloudSourceProtocol {
+    @Injected private var networkProvider: NetworkProvider
+    
+    func getPokemonGenerationI<T>() -> AnyPublisher<T, NetworkException> where T: Decodable {
+        return networkProvider.agent.run(PokeAPI.pokedexNationalKanto())
+    }
+    
+}
