e312de76e49724b38cc13f09d10406a13b27c693
Miguel Angel Zapata <mzapatae@icloud.com>
2021-04-30 20:01:10 -0400

    Register dependencies for data layer
    Signed-off-by: Miguel Angel Zapata <mzapatae@icloud.com>

--- Modules/Listing/Sources/DIComponents.swift
+++ Modules/Listing/Sources/DIComponents.swift
@@ -6,10 +6,17 @@
 //
 
 import Resolver
+import AltairMDKProviders
 
 final public class DIComponents {
     
     public static func bind() {
+    
+        // MARK: Data layer components
+        Resolver.register { NetworkProvider(strategy: .nsUrlSession) }.implements(NetworkProviderProtocol.self)
+        Resolver.register { ListingCloudSource() }.implements(ListingCloudSourceProtocol.self)
+        Resolver.register { ListingRepository() }.implements(ListingRepositoryProtocol.self)
+        Resolver.register { ListingCloudMapper.mapModelToEntity }
         
         // MARK: Domain layer components
         Resolver.register { (_, args) in GetPokemon(by: args()) }
