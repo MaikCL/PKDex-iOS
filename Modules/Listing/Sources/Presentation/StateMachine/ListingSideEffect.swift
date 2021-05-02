1195da9658669ba2f3e8e6e39ce63ec24c4b9806
Miguel Angel Zapata <mzapatae@icloud.com>
2021-05-02 19:05:39 -0400

    Added state machine components
    - Added Action
    - Added Reducer
    - Added Side effects
    - Added State
    
    Signed-off-by: Miguel Angel Zapata <mzapatae@icloud.com>

---
+++ Modules/Listing/Sources/Presentation/StateMachine/ListingSideEffect.swift
@@ -0,0 +1,45 @@
+//
+//  ListingSideEffect.swift
+//  
+//
+//  Created by Miguel Angel on 02-05-21.
+//
+
+import AltairMDKCommon
+import Resolver
+import Combine
+
+final class ListingSideEffects {
+    @LazyInjected private var getPokemonUseCase: GetPokemon
+    
+    func whenInput(action: AnyPublisher<ListingAction, Never>) -> SideEffect<ListingAction> {
+        SideEffect { _ in action }
+    }
+    
+    func whenSearchPokemon() -> SideEffect<ListingAction> {
+        SideEffect { action -> AnyPublisher<ListingAction, Never> in
+            guard case let .searchPokemon(generation) = action else { return Empty().eraseToAnyPublisher() }
+            self.$getPokemonUseCase.args = generation
+            return self.getPokemonUseCase
+                .execute()
+                .map { .searchedPokemonSuccess($0) }
+                .replaceEmpty(with: .searchedPokemonFailed(ListingException.noResults))
+                .catch { Just(.searchedPokemonFailed($0 as? Exception ?? ListingException.unknown($0))) }
+                .handleEvents(receiveOutput: { input in
+                    guard case let .searchedPokemonFailed(exception) = input else { return }
+                    self.logException(exception)
+                })
+                .eraseToAnyPublisher()
+        }
+    }
+
+}
+
+extension ListingSideEffects {
+    
+    private func logException(_ exception: Exception) {
+        // TODO: Implement apropiated record of exception
+        print("An exception occurred: [\(exception.code)] \(exception.localizedDescription)")
+    }
+    
+}
