7a45a1bc49a9fd3fc587552726a7c58411d163c8
Miguel Angel Zapata <mzapatae@icloud.com>
2021-05-19 20:50:09 -0400

    Update cloud mapper to get the pokemon ID
    Signed-off-by: Miguel Angel Zapata <mzapatae@icloud.com>

--- Modules/Listing/Sources/Data/DataSources/Cloud/ListingCloudMapper.swift
+++ Modules/Listing/Sources/Data/DataSources/Cloud/ListingCloudMapper.swift
@@ -12,10 +12,12 @@ class ListingCloudMapper: EntityFailableMapper {
     typealias Entity = [Pokemon]
     
     static var mapModelToEntity: (ListingCloudModel) throws -> [Pokemon] = { model in
+        let regexPattern = "(?<=pokemon/)[^/]+"
         guard let results = model.results else { throw MapperException.cantMapToEntity }
         return results.compactMap { result -> Pokemon? in
+            guard let id = Int(Regex.matches(for: regexPattern, in: result.url ?? "").first ?? "") else { return nil }
             guard let name = result.name else { return nil }
-            return Pokemon(name: name)
+            return Pokemon(id: id, name: name)
         }
     }
     
